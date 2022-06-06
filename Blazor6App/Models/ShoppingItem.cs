f592eea3214e0a8c51be8c810f4776c94d79eaa5
Derek Peacock <derek.peacock@yahoo.com>
2022-06-06 19:54:56 +0100

    Add project files.

---
+++ Blazor6App/Models/ShoppingItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Blazor6App.Models
+{
+	public class ShoppingItem
+	{
+		public int Id { get; set; }
+		
+		[Required, StringLength(15, ErrorMessage ="Name should be less than 15 characters!")]
+		public string Name { get; set; } = String.Empty;
+
+		[Required, Range(0.01, 100)]
+		public decimal Price { get; set; }
+
+		[Required, Range(1, 10)]
+		public int Quantity { get; set; } = 1;
+	}
+}
---
+++ Blazor6App/Services/IFileUploadService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Blazor6App.Services
+{
+    public interface IFileUploadService
+    {
+        Task UploadFile(IBrowserFile file);
+    }
+
+    public class FileUploadService : IFileUploadService
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUploadService> _logger;
+
+        public FileUploadService(IWebHostEnvironment webHostEnvironment, 
+            ILogger<FileUploadService> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+        public async Task UploadFile(IBrowserFile file)
+        {
+            if(file is not null)
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath,
+                        "uploads", file.Name);
+                    
+                    using(var stream = file.OpenReadStream())
+                    {
+                        var fileStream = File.Create(uploadPath);
+                        await stream.CopyToAsync(fileStream);
+                        fileStream.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    _logger.LogError(e.ToString());
+                }
+            }
+        }
+    }
+}
